1438a12ce76d1c588b02162b0e8ca0169ea780e1
origbu <samuelbrownadasa@gmail.com>
2022-09-09 08:49:41 -0700

    combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,20 +5,18 @@
  * Return: 0 (sucess)
  */
 int main(void)
-
 {
-
 	int d;
-
-	char low;
 
 	for (d = '0'; d <= '9'; d++)
 	{
-		putchar(d"\n");
+		putchar(d);
+		if (d != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	for (low = 'a'; low <= 'f'; low++)
-	{
-		putchar(low "\n");
-	}
+	putchar('\n');
 	return (0);
 }
