db54c96d7cf520b1a57640a5ab2419db89096fed
origbu <samuelbrownadasa@gmail.com>
2022-09-09 01:16:15 -0700

    size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,17 @@
  */
 	int main(void)
 {
-	printf("sizeof int data type : %d\n", sizeof(int));
-	printf("sizeof char data type : %d\n", sizeof(char));
-	printf("sizeof float data type : %d\n", sizeof(float));
+	int a;
+	char b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("The size of int is %lu byte(s)\n", sizeof(a));
+	printf("The size of char is %lu byte(s)\n", sizeof(b));
+	printf("The size of long int is %lu byte(s)\n", sizeof(c));
+	printf("The size of long long int is %lu byte(s)\n", sizeof(d));
+	printf("The size of float is %lu byte(s)\n", sizeof(e));
 	return (0);
 }
 
