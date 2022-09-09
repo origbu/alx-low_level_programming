df0b6080b41bd0830ae9218fac6be6d0f3e438e4
origbu <samuelbrownadasa@gmail.com>
2022-09-08 21:59:58 -0700

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - starting of the program
+ * sizeof - compile-time unary operator
+ *
+ * Return: 0 (sucess)
+ */
+	int main(void)
+{
+	printf("sizeof int data type : %d\n", sizeof(int));
+	printf("sizeof char data type : %d\n", sizeof(char));
+	printf("sizeof float data type : %d\n", sizeof(float));
+	printf("sizeof double data type : %d\n", sizeof(double));
+	return (0);
+}
+
