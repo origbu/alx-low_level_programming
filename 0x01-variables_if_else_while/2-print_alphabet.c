bc8591ef42d80bc3e3f548d38c5f4250e1c73436
origbu <samuelbrownadasa@gmail.com>
2022-09-09 07:59:49 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - starting of the code
+ *
+ * Return: 0 (sucess)
+ */
+int main(void)
+
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low);
+	putchar('\n');
+	return (0);
+}
