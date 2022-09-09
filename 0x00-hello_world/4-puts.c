eab0dfd461cf9329b4013212d3fdfe1bf45f912c
origbu <samuelbrownadasa@gmail.com>
2022-09-08 17:15:54 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
-  *main prints using puts() function
-
-  *Return value is 0
+	*main prints using puts() function
+	
+	*Return value is 0
 */
 
-int main(void)
+
+	int main()
 {
-          puts("\"Programming is like building a multilingual puzzle\n");
-
-          return (0);
-
-}	
+	puts("\"Programming is like building a multilingual puzzle");
+	
+	return 0;
+}
