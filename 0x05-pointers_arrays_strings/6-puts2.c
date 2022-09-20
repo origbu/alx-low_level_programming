bc7ec3b52090bbbb77d9f4490a6c26f0d0e701cd
origbu <samuelbrownadasa@gmail.com>
2022-09-20 14:25:32 -0700

    characters of strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,12 +10,15 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] = '\0'; i++)
+	int i = 0;
+
+	while (str[i] != '\0')
 	{
 		if ((i % 2) == 0)
 		{
 			_putchar(str[i]);
 		}
+		i++;
 	}
 	_putchar('\n');
 }
