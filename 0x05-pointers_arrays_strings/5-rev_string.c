b423c920554447e5132c41036884d66e556905da
origbu <samuelbrownadasa@gmail.com>
2022-09-20 13:51:38 -0700

    reversing strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,15 +4,16 @@
  *
  * Return:strings in reverse
  */
-
+#include "main.h"
 void rev_string(char *s)
 {
 	char rev = s[0];
 	int fcounter = 0;
 	int i;
-	
+
 	while (s[fcounter] != '\0')
 	{
+
 		fcounter++;
 	}
 	for (i = 0; i < fcounter; i++)
