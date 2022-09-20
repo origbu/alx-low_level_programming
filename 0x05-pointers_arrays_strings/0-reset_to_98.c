88c5638ac52036b451401cae671a7529f326754b
origbu <samuelbrownadasa@gmail.com>
2022-09-19 21:47:52 -0700

    creating a function

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+/**
+ * reset_to_98-a function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98.
+ * @n:a variable used to store integer
+ *
+ * Return:void
+ */
+#include "main.h"
+
+void reset_to_98(int *n)
+{
+	int n;
+
+	n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+This project is to get students accustomed to working with pointers and arrays
