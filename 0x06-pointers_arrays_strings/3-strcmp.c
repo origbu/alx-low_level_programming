f52437d0a2a87555d1e9d1ae5582bc01461a5374
origbu <samuelbrownadasa@gmail.com>
2022-09-21 19:47:53 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,7 +3,7 @@
  * @s1:first string to be compared
  * @s2:second string to be compared
  *
- * Return:dest
+ * Return:R
  */
 #include "main.h"
 
@@ -13,7 +13,7 @@ int _strcmp(char *s1, char *s2)
 
 	int i = 0;
 
-	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
+	while ((s1[i] == s2[i] && s1[i] != '\0' || s2[i] != '\0'))
 	{
 		i++;
 	}
