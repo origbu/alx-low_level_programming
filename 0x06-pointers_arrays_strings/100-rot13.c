6cfef4d53d2598948fde11894796e9f4ba368ec2
origbu <samuelbrownadasa@gmail.com>
2022-09-21 13:32:03 -0700

    encoding strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+/**
+ * rot13-a function that encodes a string
+ * @s:input string
+ *
+ * Return:ptr
+ */
+#include "main.h"
+
+char *rot13(char *s)
+{
+	int i;
+
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *ptr = s;
+
+	while (*s)
+	{
+		for (i = 0; i <= 52; i++)
+		{
+			if (*s == rot13[i])
+			{
+				*s = ROT13[i];
+				break;
+			}
+		}
+		s++;
+	}
+	return (ptr);
+}
