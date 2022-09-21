ef77ca88d172ff095bb917ccbd9589329a033d22
origbu <samuelbrownadasa@gmail.com>
2022-09-20 17:07:37 -0700

    copying arrays

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+/**
+ * *_strcpy-a function that copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by des
+ * @dest:pointer to be copied to
+ * @src:pointer to be copied from
+ *
+ * Return:dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int k;
+
+	for (k = 0; src[k] != '\0'; k++)
+	{
+		src[k] = dest[k];
+	}
+	return (dest);
+}
