cc0992f696b9ca023f3f9298d648095db2236091
origbu <samuelbrownadasa@gmail.com>
2022-09-21 11:42:21 -0700

    comparing strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+/**
+ * _strcmp-a function that compares two strings
+ * @s1:first string to be compared
+ * @s2:second string to be compared
+ *
+ * Return:R
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, R;
+
+	int i = 0;
+
+	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
+	{
+		i++;
+	}
+	R = s1[i] - s2[i];
+	return (R);
+}
