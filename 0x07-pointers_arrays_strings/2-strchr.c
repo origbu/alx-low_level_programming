01fda6c1cc81548aeb9d7f2c1119dbbf6e1d6aa2
origbu <samuelbrownadasa@gmail.com>
2022-09-27 10:14:51 -0700

    getting characters

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,18 +10,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	int a;
+	int i = 0;
 
-	while (1)
-	{
-		a = *s++;
-		if (a == c)
-		{
-			return (s - 1);
-		}
-		if (a == 0)
-		{
-			return (NULL);
-		}
-	}
+	while (s[i] != '\0' && s[i] != c)
+		i++;
+	if (s[i] == c)
+		return (&s[i]);
+	else
+		return('\0');
 }
