917b1cec563fd6c6dd63b571044bf8b321c8c3ee
origbu <samuelbrownadasa@gmail.com>
2022-09-30 15:49:50 +0100

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,7 +2,7 @@
  * factorial-a function that returns the factorial of a given number
  * @n:number to get it's factorial
  *
- * Return:factorial of n
+ * Return:factorial of n or -1 if n is negative.
  */
 #include "main.h"
 
