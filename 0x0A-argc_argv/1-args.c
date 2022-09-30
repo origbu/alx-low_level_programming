098a1b5939747c53c6af0005a0ab47dcef676be0
origbu <samuelbrownadasa@gmail.com>
2022-09-30 13:04:44 -0700

    argument counter

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+/**
+ * main-a program that prints the number of arguments passed into it.
+ * @argc:argument counter
+ * @argv:pointer to arguments passed to main
+ *
+ * Return:0
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%d\n", i);
+	return (0);
+}
