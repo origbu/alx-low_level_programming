d96e16733931dea92a5da3db86f758f7528d996a
origbu <samuelbrownadasa@gmail.com>
2022-09-30 20:50:29 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,8 +9,9 @@
  * Return:0
  */
 
-int main(argc __atrribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-		printf("%s\n", *argv);
-	return (0);
+	if ( argc > 0)
+		printf("%s\n", argv[0])
+		return (0);
 }
