4f4909ab0e0b28a772d5740fe8a500704c38752d
origbu <samuelbrownadasa@gmail.com>
2022-10-01 23:10:32 +0100

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -14,67 +14,67 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[])
-
 {
+int n, coins = 0;
 
-	int n, coins = 0;
+/* validate input */
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
 
+if (argv[1][0] == '-')
+{
+printf("0\n");
+return (0);
+}
 
+/* convert string to int and calculate coins */
+n = atoi(argv[1]);
 
-	/* validate input */
+coins += n / 25;
+n = n % 25;
+coins += n / 10;
+n = n % 10;
+coins += n / 5;
+n = n % 5;
+coins += n / 2;
+n = n % 2;
+coins += n / 1;
 
-	if (argc != 2)
+printf("%d\n", coins);
+return (0);
+int main(int argc, char *argv[])
+{
+int n, coins = 0;
 
-	{
+/* validate input */
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
 
-		printf("Error\n");
+if (argv[1][0] == '-')
+{
+printf("0\n");
+return (0);
+}
 
-		return (1);
+/* convert string to int and calculate coins */
+n = atoi(argv[1]);
 
-	}
+coins += n / 25;
+n = n % 25;
+coins += n / 10;
+n = n % 10;
+coins += n / 5;
+n = n % 5;
+coins += n / 2;
+n = n % 2;
+coins += n / 1;
 
-
-
-	if (argv[1][0] == '-')
-
-	{
-
-		printf("0\n");
-
-		return (0);
-
-	}
-
-
-
-	/* convert string to int and calculate coins */
-
-	n = atoi(argv[1]);
-
-
-
-	coins += n / 25;
-
-	n = n % 25;
-
-	coins += n / 10;
-
-	n = n % 10;
-
-	coins += n / 5;
-
-	n = n % 5;
-
-	coins += n / 2;
-
-	n = n % 2;
-
-	coins += n / 1;
-
-
-
-	printf("%d\n", coins);
-
-	return (0);
-
+printf("%d\n", coins);
+return (0);
 }
