f15140a0456955c97b645d281235257c4017446d
origbu <samuelbrownadasa@gmail.com>
2022-10-06 18:59:35 +0100

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,28 +14,22 @@
 
 char *_strdup(char *str)
 {
-	char *nstr;
-	unsigned int i, len;
+	unsigned int i, j;
+	char *s;
 
-	if (nstr == NULL)
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		;
+	i++;
+	s = malloc(i * sizeof(char));
+	if (s == NULL)
 	{
 		return (NULL);
 	}
-	len = 0;
-	while (str[len] != '\0')
+	for (j = 0; j < i; j++)
 	{
-		len++;
+		s[j] = str[j];
 	}
-	nstr = malloc(sizeof(char) * (len + 1));
-
-	if (nstr == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < len; i++)
-	{
-		nstr[i] = str[i];
-	}
-	nstr[len] = '\0';
-	return (nstr);
+	return (s);
 }
