8b552da288819fa40e4c48a3b5c90a75266fce01
origbu <samuelbrownadasa@gmail.com>
2022-10-09 09:48:16 +0100

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,12 +20,12 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	**a = (int *)malloc(sizeof(int *) * height);
+	a = (int **)malloc(sizeof(int *) * height);
 	if (a == 0)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		a[i] = malloc(sizeof(int) * width);
+		a[i] =(int *) malloc(sizeof(int) * width);
 		if (a[i] == NULL)
 		{
 			while (i >= 0)
