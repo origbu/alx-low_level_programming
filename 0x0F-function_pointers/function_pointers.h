76975296c6ff7f02291e1c9670c89714c898346b
origbu <samuelbrownadasa@gmail.com>
2022-10-12 21:25:10 +0100

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,10 @@
 #ifndef __FUNCTION_POINTERS__
 #define __FUNCTION_POINTERS__
 
+#include <stdlib.h>
+#include <stddef.h>
+
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
