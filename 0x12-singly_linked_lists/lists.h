fe1a50520787eb6b774e1da1250320f0f8acf67e
origbu <samuelbrownadasa@gmail.com>
2022-10-20 14:16:09 -0700

    header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _LISTS_
+#define _LISTS_
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
